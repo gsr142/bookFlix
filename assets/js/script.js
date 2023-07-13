0d67e3450dbe5556d8fb20251b0c3f1e6f77cc42
virginiafreitas <virginiacdefreitas@gmail.com>
2023-07-13 14:55:23 -0700

    added style with Bulma

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,18 @@ var booksAPI = "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:k
 var streamKey = "CmuYlyfZgLsyiWCJA2h829wGh6WRb77CHmcowDQP"
 var streamingAPI = "https://api.watchmode.com/v1/list-titles/?apiKey=" + streamKey + "&genres=horror&sort_by=relevence_desc,popularity_desc"
 
-fetch(streamingAPI)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    })
+// fetch(booksAPI)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function(data){
+//         console.log(data);
+//     })
+
+// fetch(streamingAPI)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function(data){
+//         console.log(data);
+//     })
