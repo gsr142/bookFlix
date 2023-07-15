1783ac3982cc20971c40962fca1ac8acf9094e47
gsr142 <gsr142@gmail.com>
2023-07-15 09:11:21 -0700

    add API values to streaming service choices

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,14 +1,21 @@
 var booksApiKey = "AIzaSyBzxk-Jd5sokQW1oRM9XJS4Np1hbEmum1I"
 var booksAPI = "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key="+booksApiKey
+
+var getChoices = localStorage.getItem('answer');
+var movieGenre = JSON.parse(getChoices)[0];
+var streamSource = JSON.parse(getChoices)[4];
+
 var streamKey = "CmuYlyfZgLsyiWCJA2h829wGh6WRb77CHmcowDQP"
-var streamingAPI = "https://api.watchmode.com/v1/list-titles/?apiKey=" + streamKey + "&genres="+movieGenre+"&sort_by=popularity_desc"
+var streamingAPI = "https://api.watchmode.com/v1/list-titles/?apiKey=" + streamKey + "&genres="+ movieGenre +"&types=movie&sources"+streamSource+"sort_by=popularity_desc"
 
-var movieGenre = localStorage.getItem('answer');
-movieGenre = JSON.parse(movieGenre)[0];
-console.log(localStorage)
+
 console.log(movieGenre)
+console.log(localStorage)
+console.log(streamingAPI)
 
-fetch(streamingAPI)
+
+function getMovie (){
+    fetch(streamingAPI)
     .then(function (response) {
         if (response.ok) {
             response.json().then(function (data){
@@ -21,3 +28,8 @@ fetch(streamingAPI)
     .catch(function(error) {
         alert('Unable to connect to the server')
     });
+}
+
+//button for testing only
+var button = document.getElementById('button')
+button.addEventListener('click', getMovie)
