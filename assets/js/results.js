24d7895ab58f470e63cf5507136f5e06aa28c9a5
gsr142 <gsr142@gmail.com>
2023-07-16 14:11:53 -0700

    movie results now display on page

--- assets/js/results.js
+++ assets/js/results.js
@@ -15,6 +15,13 @@ $(document).ready(function() {
     var releaseDateEnd = (Number(movieYear)) + 9
     releaseDateEnd = releaseDateEnd.toString() + "1231"
 
+    var movieDisplayArr1 = ['#result1-movies-image', '#result1-title', '#result1-stars', '#result1-genre', '#result1-description'];
+    var movieDisplayArr2 = ['#result2-movies-image', '#result2-title', '#result2-stars', '#result2-genre', '#result2-description']
+    var movieDisplayArr3 = ['#result3-movies-image', '#result3-title', '#result3-stars', '#result3-genre', '#result3-description']
+    var movieDisplayArr4 = ['#result4-movies-image', '#result4-title', '#result4-stars', '#result4-genre', '#result4-description']
+
+
+
 
     // API key and url for Watchmode. Search by year and genre to get title suggestions for each streaming service
     var streamKey = "CmuYlyfZgLsyiWCJA2h829wGh6WRb77CHmcowDQP"
@@ -29,11 +36,19 @@ $(document).ready(function() {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data){
-                    var movieTitle = data.titles[0].title
-                    var imdbId = data.titles[0].imdb_id
-                    console.log(imdbId);
-                    //getPoster(imdbId);
-                    //getMovieInfo(imdbId);
+                    
+                    var imdbId1 = data.titles[0].imdb_id
+                    var imdbId2 = data.titles[1].imdb_id
+                    var imdbId3 = data.titles[2].imdb_id
+                    var imdbId4 = data.titles[3].imdb_id
+                    console.log(imdbId1)
+                    console.log(imdbId2)
+                    console.log(imdbId3)
+                    console.log(imdbId4)
+                    getMovieInfo(imdbId1, movieDisplayArr1);
+                    getMovieInfo(imdbId2, movieDisplayArr2);
+                    getMovieInfo(imdbId3, movieDisplayArr3);
+                    getMovieInfo(imdbId4, movieDisplayArr4)
 
                 });
             } else {
@@ -44,24 +59,26 @@ $(document).ready(function() {
             alert('Unable to connect to the server')
         });
     }
-    function getMovieInfo (imdbId) {
-        omdbInfo = omdbInfo+imdbId;
-        fetch(omdbInfo)
+    function getMovieInfo (imdbId, arr) {
+        var getOmdbInfo = omdbInfo+imdbId;
+        console.log(getOmdbInfo)
+        fetch(getOmdbInfo)
             .then(function (response){
                 if (response.ok) {
                     response.json().then(function (data){
                         console.log(data)
-                        $('#result1-movies-image').attr('src', data.Poster);
-                        $('#result1-title').text(data.Title);
-                        $('#result1-stars').text('Starring: '+data.Actors);
-                        $('#result1-genre').text('Genre: '+data.Genre);
-                        $('#result1-description').text('Plot: '+data.Plot)
+                        $(arr[0]).attr('src', data.Poster);
+                        $(arr[1]).text(data.Title);
+                        $(arr[2]).text('Starring: '+data.Actors);
+                        $(arr[3]).text('Genre: '+data.Genre);
+                        $(arr[4]).text('Plot: '+data.Plot)
                     });
                 }
             });
     }
-    getMovieInfo('tt3783958')
-
+    // var movieDisplayArr1 = ['#result1-movies-image', '#result1-title', '#result1-stars', '#result1-genre', '#result1-description']
+    // var testId = 'tt3783958'
+    // getMovieInfo(testId, movieDisplayArr)
    
 
     //button for testing only. will be automatic on deployment
